1c22c506f5bc111fa08f95f17fa58ead743ccb01
Snegopad-izh <snegopad-izh@rambler.ru>
2022-07-04 18:05:53 +0400

    feat: reducers

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,12 +1,12 @@
+import profileReducer from "./profile-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import sidebarReducer from "./sidebar-reducer";
+
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
-
-
-
-
 
 let store = {
     _state: {
@@ -40,72 +40,34 @@ let store = {
             ],
             newPostText: 'it_specialist',
         },
+
+        sidebar: {},
     },
 
     _callSubscriber() {
         console.log('State changed');
     },
-
-    getState() {
-        return this._state;
-    },
     subscribe(observer) {
         this._callSubscriber = observer;
     },
 
+    getState() {
+        return this._state;
+    },
+
     dispatch(action) {
-        if (action.type === ADD_POST) {
-            let newPost = {
-                id: 7,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0,
-            };
+        this._state.profilePage = profileReducer(this._state.profilePage, action);
+        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+        this._state.sidebar = sidebarReducer(this._state.sidebar, action);
 
-            this._state.profilePage.posts.push(newPost);
-
-            /* зануляем текстовое поле после сохранения в state */
-            this._state.profilePage.newPostText = '';
-
-            /* отрисовываем */
-            this._callSubscriber(this._state);
-        }
-
-        else if (action.type === UPDATE_NEW_POST_TEXT) {
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubscriber(this._state);
-        }
-
-        else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-            this._state.dialogsPage.newMessageBody = action.body;
-            this._callSubscriber(this._state);
-        }
-
-        else if (action.type === SEND_MESSAGE) {
-            let body = this._state.dialogsPage.newMessageBody;
-            this._state.dialogsPage.newMessageBody = '';
-            this._state.dialogsPage.messages.push({id: 6, message: body});
-            this._callSubscriber(this._state);
-        }
-
+        this._callSubscriber(this._state);
     }
 
 };
 
 
 
-export const addPostActionCreator = () => ({type: ADD_POST});
 
-export const  updateNewPostActionCreator = (text) => ({
-    type: UPDATE_NEW_POST_TEXT,
-    newText: text,
-});
-
-export const sendMessageCreator = () => ({type: SEND_MESSAGE});
-
-export const  updateNewMessageBodyCreator = (body) => ({
-    type: UPDATE_NEW_MESSAGE_BODY,
-    body: body,
- });
 
 
 
